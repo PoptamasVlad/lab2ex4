1e23a36c4010b8395a7078e50a26dbe5f8a58fef
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-04 15:44:43 +0200

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va determina daca un numar este sau nu
+            palindrom. */
+            int n = int.Parse(Console.ReadLine());
+            int p = n, x = 0;
+            while(n != 0)
+            {
+                x = x * 10;
+                x = x + n % 10;
+                n = n / 10;
+            }
+            if(x == p)
+            {
+                Console.WriteLine("Este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Nu este palindrom");
+            }    
+        }
+    }
+}
